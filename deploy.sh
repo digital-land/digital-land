8511963f276c9eccdee4cac98b6babe5217e6dfd
Adam Shimali <adam.shimali@gmail.com>
2018-10-12 10:26:08 +0100

    Do the submodule stuff using the hugo docs way but do not do submodule init, but instead ignore the public directory and create submodule with -f

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,20 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
 # Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-
-git add .
-
 git commit -m "$msg"
 
 # Push source and build repos.
-git push --recurse-submodules=on-demand
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
